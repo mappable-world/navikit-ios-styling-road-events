1363f30cf5a10bcec2f4eb7802ef68d27d2e7d25
MappableWorld <tech-mobile@mappable.world>
2025-02-18 15:36:23 +0300

    Publish swift package MMKStylingRoadEvents version 4.11.0

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version: 5.6
+
+import PackageDescription
+
+let package = Package(
+    name: "MMKStylingRoadEvents",
+    defaultLocalization: "en",
+    platforms: [.iOS("13.0")],
+    products: [
+        .library(
+            name: "MMKStylingRoadEvents",
+            type: .static,
+            targets: [
+                "MMKStylingRoadEvents",
+                "MMKStylingRoadEventsResources"
+            ]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/mappable-world/navikit-ios", exact: "4.11.0"),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "MMKStylingRoadEvents",
+            url: "https://mappableworld.s3.eu-west-3.amazonaws.com/MMKStylingRoadEvents-4.11.0.framework.zip",
+            checksum: "c8143b00cd814600e4f379880b50c91f97ef088e5abe319bd3807d655d0ad090"
+        ),
+        .target(
+            name: "MMKStylingRoadEventsResources",
+            dependencies: [
+                .product(name: "MappableMobileNavikit", package: "navikit-ios"),
+            ],
+            path: "Resources",
+            resources: [.process("Contents")]
+        )
+    ]
+)
---
+++ Resources/Resources.m
@@ -0,0 +1,13 @@
+#import <resource_bundle_accessor.h>
+
+@interface MMKStylingRoadEventsSwiftResources : NSObject
+@end
+
+@implementation MMKStylingRoadEventsSwiftResources
+
++ (NSBundle *)mapkitResourcesBundle
+{
+    return SWIFTPM_MODULE_BUNDLE;
+}
+
+@end
