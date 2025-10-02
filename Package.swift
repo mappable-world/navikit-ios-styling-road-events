683faf705dbafa46756520037a697b1b39f6cd9d
MappableWorld <tech-mobile@mappable.world>
2025-10-02 13:12:42 +0300

    Publish swift package MMKStylingRoadEvents version 4.23.0

--- Package.swift
+++ Package.swift
@@ -17,13 +17,13 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/mappable-world/navikit-ios", exact: "4.22.0"),
+        .package(url: "https://github.com/mappable-world/navikit-ios", exact: "4.23.0"),
     ],
     targets: [
         .binaryTarget(
             name: "MMKStylingRoadEvents",
-            url: "https://mappableworld.s3.eu-west-3.amazonaws.com/MMKStylingRoadEvents-4.22.0.framework.zip",
-            checksum: "efd4575960bba497b90b5f1483782ede2de0e9e899ca2c14911b50947207a426"
+            url: "https://mappableworld.s3.eu-west-3.amazonaws.com/MMKStylingRoadEvents-4.23.0.framework.zip",
+            checksum: "28b21bcb19e06a1ee44eccc23086227737f1ac7bf7df7f37767ca56a7a495a82"
         ),
         .target(
             name: "MMKStylingRoadEventsResources",
