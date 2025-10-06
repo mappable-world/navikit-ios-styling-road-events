fe936b66d917a4957c395bb627ee5ef7f559ebb9
MappableWorld <tech-mobile@mappable.world>
2025-10-06 18:37:26 +0300

    Publish swift package MMKStylingRoadEvents version 4.23.0

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         .binaryTarget(
             name: "MMKStylingRoadEvents",
             url: "https://mappableworld.s3.eu-west-3.amazonaws.com/MMKStylingRoadEvents-4.23.0.framework.zip",
-            checksum: "28b21bcb19e06a1ee44eccc23086227737f1ac7bf7df7f37767ca56a7a495a82"
+            checksum: "968e9347cb1e78b65211163d3aba5d346ef938eac406274b69957bc816477118"
         ),
         .target(
             name: "MMKStylingRoadEventsResources",
